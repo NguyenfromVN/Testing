32e05f05ba7ee44a2cd311c597708fb096cc3e61
NguyenfromVN <baonguyentqt@gmail.com>
2020-12-27 19:34:34 +0700

    Update Diem.js

--- Diem.js
+++ Diem.js
@@ -6,7 +6,7 @@ class Diem {
 
     distance(p) {
         const dx = this.x - p.x;
-        const dy = this.y - p.y - 0.0001; // make some weird things
+        const dy = this.y - p.y - 0.00000000001; // make some weird things
 
         return Math.hypot(dx, dy);
     }
